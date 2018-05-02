ce304b97b71b58ad459dab3389ab4f74dbf4fa70
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2018-05-02 21:19:03 +0200

    VortexWeb3Loading and VortexMetamaskLoader usage
    VTXD ?

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import {VortexGate, VortexWeb3Loaded, VortexWeb3LoadError, VortexWeb3NetworkError} from "vort_x-components";
+import {VortexGate, VortexWeb3Loaded, VortexWeb3LoadError, VortexWeb3NetworkError, VortexWeb3Loading, VortexMetamaskLoader} from "vort_x-components";
 import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
-import getWeb3 from './utils/getWeb3'
 import {TransactionPanel} from "./demo-sections/transactions";
 import {Grid, Row, Col} from 'react-bootstrap';
+import Web3 from 'web3';
 
 import './css/oswald.css'
 import './css/open-sans.css'
@@ -23,7 +23,7 @@ class App extends Component {
 
     render() {
         return (
-            <VortexGate contracts={[SimpleStorageContract]} network_contracts={[SimpleStorageContract]} loader={getWeb3}>
+            <VortexGate contracts={[SimpleStorageContract]} network_contracts={[SimpleStorageContract]} loader={VortexMetamaskLoader(Web3)}>
                 <VortexWeb3Loaded>
                     <FeedNotifications>
                         <div className="App">
@@ -48,6 +48,21 @@ class App extends Component {
                         </div>
                     </FeedNotifications>
                 </VortexWeb3Loaded>
+                <VortexWeb3Loading>
+                    <div className="App">
+                        <nav className="navbar pure-menu pure-menu-horizontal">
+                            <a href="#" className="pure-menu-heading pure-menu-link">VortΞx Demo</a>
+                        </nav>
+
+                        <main className="container">
+                            <div className="pure-g">
+                                <div className="pure-u-1-1">
+                                    <h1>Loading ... </h1>
+                                </div>
+                            </div>
+                        </main>
+                    </div>
+                </VortexWeb3Loading>
                 <VortexWeb3LoadError>
                     <div className="App">
                         <nav className="navbar pure-menu pure-menu-horizontal">
