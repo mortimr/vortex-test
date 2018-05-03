bcecc9617238a3a414be93e726e638345744c3bd
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2018-05-03 11:08:09 +0200

    Left pane overlay dispkays feed
    VTXD

--- src/demo-sections/left-overlay/index.js
+++ src/demo-sections/left-overlay/index.js
@@ -3,6 +3,43 @@ import Modal from 'react-modal';
 import SlidingPane from 'react-sliding-pane';
 import {Panel, Button} from 'react-bootstrap';
 import 'react-sliding-pane/dist/react-sliding-pane.css';
+import {VortexFeedList, FeedType} from 'vort_x-components';
+
+class ListElement extends Component {
+    constructor(props) {
+        super(props);
+        this.title = "";
+        this.body = "";
+        this.style = "danger";
+        switch (this.props.data.action) {
+            case 'NEW_CONTRACT':
+                this.title = "New Contract";
+                this.body = "Contract " + this.props.data.contract_name + " at address " + this.props.data.contract_address + " has been loaded.";
+                this.style = "primary";
+                break;
+            case 'NEW_TRANSACTION':
+                this.title = "New Transaction";
+                this.body = "Transaction " + this.props.data.transaction_hash + " has been broadcasted";
+                this.style = "success";
+                break;
+            case 'NEW_ERROR':
+                this.title = "New Error";
+                this.body = this.props.data.error.message;
+                break;
+            default:
+                this.title = "Unknown action";
+        }
+
+    }
+    render() {
+        return <Panel bsStyle={this.style}>
+            <Panel.Heading>{this.title}</Panel.Heading>
+            <Panel.Body style={{wordWrap: 'break-word'}}>
+                <p>{this.body}</p>
+            </Panel.Body>
+        </Panel>
+    }
+}
 
 export class Overlay extends Component {
     constructor(props) {
@@ -19,7 +56,7 @@ export class Overlay extends Component {
 
     render() {
         return <Panel>
-            <Panel.Heading>Press this magic button !</Panel.Heading>
+            <Panel.Heading >Press this magic button !</Panel.Heading>
             <Panel.Body>
                 <p>You can easily access all the data created by your user's activity. This is a great tool to give more informations to the user about what is happening behind the scene.</p>
                 <div ref={ref => this.el = ref}>
@@ -30,9 +67,9 @@ export class Overlay extends Component {
                         isOpen={ this.state.isPaneOpenLeft }
                         title='VortΞx Transaction Summary'
                         from='left'
-                        width={window.innerWidth * 0.2}
+                        width={(Math.floor(window.innerWidth * 0.3)).toString() + "px"}
                         onRequestClose={ () => this.setState({ isPaneOpenLeft: false }) }>
-                        <div>And I am pane content on left.</div>
+                        <VortexFeedList filter={FeedType.Contracts | FeedType.Errors | FeedType.Transactions} element={ListElement}/>
                     </SlidingPane>
                 </div>
             </Panel.Body>
