60242a10e0501287fa43df76077e8db0b4ae240f
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2018-05-04 23:32:18 +0200

    Update display titles
    VTXC

--- src/demo-sections/list-transactions/index.js
+++ src/demo-sections/list-transactions/index.js
@@ -6,17 +6,22 @@ class TxContainer extends React.Component {
     render() {
         if (this.props.tx) {
             let status = "warning";
+            let status_title;
             switch (this.props.tx.status.type) {
                 case 'BROADCASTED':
                     status = "primary";
+                    status_title = "Transaction has been broadcasted";
                     break;
                 case 'CONFIRMED':
+                    status = "success";
+                    status_title = "Transaction has been confirmed " + this.props.tx.status.transaction_confirmation_count + " times";
                     break;
                 case 'RECEIPT':
-                    status = "success";
+                    status_title = "Transaction receipt has been received, waiting for confimations";
                     break ;
                 case 'ERROR':
                     status = "danger";
+                    status_title = "Error occured while doing transaction";
                     break ;
                 default:
                     status = "danger";
@@ -25,7 +30,7 @@ class TxContainer extends React.Component {
             return <Panel bsStyle={status}>
                 <Panel.Heading>{this.props.tx.status.transaction_hash}</Panel.Heading>
                 <Panel.Body>
-                    <h4>{this.props.tx.status.type}</h4>
+                    <h4>{status_title}</h4>
                     <p>Transaction
                         from {this.props.tx.transaction_arguments.from} to {this.props.tx.transaction_arguments.to} for {parseInt(this.props.tx.transaction_arguments.value, 16)} WΞI</p>
                 </Panel.Body>
